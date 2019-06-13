a79acc867dde5814c25f89dffe538c2c80b36545
parsonsbots <parsonsbots@gmail.com>
2019-06-12 17:37:05 -0700

    Remove unused argument in processPagination

--- src/php/resources/js/script.js
+++ src/php/resources/js/script.js
@@ -27,7 +27,7 @@ var onLoad = (callback) => {
 	document.readyState != 'complete' ? setTimeout('onLoad(' + callback + ')', 1) : callback();
 };
 
-var processPagination = (currentPage, selected) => {
+var processPagination = (currentPage) => {
 	var checkboxAll = document.querySelector('.checkbox.all'),
 		items = document.querySelector('.proxy-configuration table'),
 		pagination = document.querySelector('.pagination');

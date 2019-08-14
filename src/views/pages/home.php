d2b53858cc4b0a38eda58de715e34199135e7cdb
parsonsbots <parsonsbots@gmail.com>
2019-08-14 16:09:37 -0700

    Homepage styling and text changes

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -7,16 +7,22 @@
 <main class="home">
 	<div class="section">
 		<div class="container small">
-			<h1>Premium Proxy Services</h1>
-			<p>Power your business and applications through the anonymous Eightomic IP network using a custom-built, <a href="https://github.com/parsonsbots/proxies" target="_blank">open-source</a> proxy control panel with granular access controls.</p>
-			<img alt="Proxy control panel screenshot" src="/resources/images/screenshots/control-panel.png">
+			<div class="feature-container">
+				<h1>Premium Proxy Services</h1>
+				<p>Power your business and applications through the anonymous Eightomic IP network using a custom-built, <a href="https://github.com/parsonsbots/proxies" target="_blank">open-source</a> proxy control panel with granular access controls.</p>
+				<a class="button main-button" href="/?#register">Get Started</a>
+				<a class="button text-button" href="/features">See Features</a>
+				<div class="feature-screenshot">
+					<img alt="Proxy control panel screenshot" src="/resources/images/screenshots/control-panel.png">
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="section">
 		<div class="container small">
 			<div class="item">
 				<h2>Private Proxies</h2>
-				<p>Buy elite (high-anonymous) private HTTP proxies with unlimited threads, fast speeds, stable uptime and unmetered data transfer.</p>
+				<p>Buy elite (high-anonymous) HTTP proxies with private access, unlimited threads, fast speeds, stable uptime and unmetered data transfer.</p>
 				<a href="/private-proxies">Buy Private Proxies</a>
 			</div>
 			<div class="item no-margin-bottom">

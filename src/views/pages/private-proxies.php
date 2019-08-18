eb1a5bd37f7bfc5b0f5c034cfc81d2e38ad582e4
parsonsbots <parsonsbots@gmail.com>
2019-08-18 15:27:18 -0700

    Add minimal pricing table to private-proxies page

--- src/views/pages/private-proxies.php
+++ src/views/pages/private-proxies.php
@@ -7,8 +7,73 @@
 <main class="private-proxies">
 	<div class="section">
 		<div class="container small">
-			<h1>Private Proxies</h1>
-			<p>[Coming soon]</p>
+			<h1>Private HTTP Proxies</h1>
+			<div class="table">
+				<table>
+					<tbody>
+						<tr>
+							<td><strong>5 proxies</strong></td>
+							<td><span class="monthly-price">$2.99 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>10 proxies</strong></td>
+							<td><span class="monthly-price">$5.98 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>15 proxies</strong></td>
+							<td><span class="monthly-price">$8.95 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>20 proxies</strong></td>
+							<td><span class="monthly-price">$11.90 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>30 proxies</strong></td>
+							<td><span class="monthly-price">$17.78 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>50 proxies</strong></td>
+							<td><span class="monthly-price">$29.40 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>100 proxies</strong></td>
+							<td><span class="monthly-price">$57.60 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>200 proxies</strong></td>
+							<td><span class="monthly-price">$110.40 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>300 proxies</strong></td>
+							<td><span class="monthly-price">$158.40 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>400 proxies</strong></td>
+							<td><span class="monthly-price">$201.60 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>500 proxies</strong></td>
+							<td><span class="monthly-price">$240.00 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+						<tr>
+							<td><strong>1000 proxies</strong></td>
+							<td><span class="monthly-price">$360.00 per month</span></td>
+							<td><a class="add-to-cart button main-button" href="javascript:void(0);">Add to Cart</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	</div>
 </main>

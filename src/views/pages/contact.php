0e61730a391fc7758705b6e3587d83377037827b
parsonsbots <parsonsbots@gmail.com>
2019-08-18 17:36:36 -0700

    Contact page

--- src/views/pages/contact.php
+++ src/views/pages/contact.php
@@ -8,7 +8,7 @@
 	<div class="section">
 		<div class="container small">
 			<h1>Contact</h1>
-			<p>[Coming soon]</p>
+			<p>Please send an email to <a href="mailto:eightomic@gmail.com">eightomic@gmail.com</a> for any pre-sales questions or support inquiries.</p>
 		</div>
 	</div>
 </main>

03d6dac741919c928f8bd84d3dfa6cc0088b6a4e
parsonsbots <parsonsbots@gmail.com>
2019-08-19 17:45:39 -0700

    Add placeholders to search fields

--- src/views/sections/search.php
+++ src/views/sections/search.php
@@ -9,9 +9,9 @@
 					</div>
 					<div class="item-body">
 						<label for="broad-search">Search Terms</label>
-						<input class="broad-search" id="broad-search" name="broad_search" type="text">
+						<input class="broad-search" id="broad-search" name="broad_search" placeholder="<?php echo "Enter broad search terms (e.g. online AS88888)"; ?>" type="text">
 						<label for="granular-search">Filter List of Specific IPs or Subnets</label>
-						<textarea class="granular-search" id="granular-search" name="granular_search"></textarea>
+						<textarea class="granular-search" id="granular-search" name="granular_search" placeholder="<?php echo "Enter list of specific proxy IPs to filter within your order\n127.0.0.1\n127.0.0.2\netc..."; ?>"></textarea>
 						<div class="checkbox-container">
 							<span checked="0" class="checkbox" id="exclude-search" name="exclude_search"></span>
 							<label class="custom-checkbox-label" for="exclude-search" name="exclude_search">Exclude proxies matching terms and filter</label>

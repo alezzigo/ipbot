e8a99ea126153fcaf6d7e7fdadc03f3cc1b6ec97
parsonsbots <parsonsbots@gmail.com>
2019-08-22 12:29:03 -0700

    Remove x-ua-compatible meta tag

--- src/views/sections/header.php
+++ src/views/sections/header.php
@@ -3,7 +3,6 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="x-ua-compatible" content="IE=edge">
 <link rel="shortcut icon" type="image/png" href="/resources/images/icons/favicon.png">
 <title><?php echo $config->parameters['title']; ?></title>
 <?php

ce57c70cac4b9969142fd193dcb670f4b4cd0e2d
parsonsbots <parsonsbots@gmail.com>
2019-09-16 16:58:04 -0700

    Add shell method processing

---
+++ src/shell.php
@@ -0,0 +1,24 @@
+<?php
+	require_once(str_replace('shell.php', 'config.php', $_SERVER['PWD'] . '/' . $_SERVER['SCRIPT_FILENAME']));
+	$output = 'Error processing shell method, please check the parameters and try again.';
+
+	if (
+		!empty($config) &&
+		!empty($table = strtolower($_SERVER['argv'][1]))
+	) {
+		require_once($config->settings['base_path'] . '/models/' . $table . '.php');
+		$shellObjectName = ucwords($table) . 'Model';
+		$shellObject = new $shellObjectName();
+
+		if (
+			!empty($shellMethod = $_SERVER['argv'][2]) &&
+			method_exists($shellObject, ($shellMethod = 'shell' . ucwords($shellMethod)))
+		) {
+			$shellObject->$shellMethod();
+			$output = 'Completed processing ' . $shellMethod . ' for ' . $table . '.';
+		}
+	}
+
+	echo $output;
+	exit;
+?>

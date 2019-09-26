defd3d31a1c33ae8bae78e8d67f5453d5c31d5df
parsonsbots <parsonsbots@gmail.com>
2019-09-26 14:45:56 -0700

    Correct class names for upgrade and downgrade windows

--- src/views/sections/downgrade.php
+++ src/views/sections/downgrade.php
@@ -2,18 +2,19 @@
 	<div class="section window">
 		<div class="item-container">
 			<div class="item">
-				<div class="search-configuration">
+				<div class="downgrade-configuration">
 					<div class="item-header">
 						<span class="button close icon icon-close"></span>
 						<h1>Request Downgrade</h1>
 					</div>
 					<div class="item-body">
 						<div class="message-container"></div>
-						<div class="upgrade-container"></div>
+						<div class="downgrade-container"></div>
+						<div class="clear"></div>
 					</div>
 					<div class="item-footer">
 						<button class="button close alternate-button">Close</button>
-						<button class="button main-button submit" process="upgrade" window="upgrade">Request Downgrade</button>
+						<button class="button main-button submit" process="downgrade" window="downgrade">Request Downgrade</button>
 					</div>
 				</div>
 			</div>
--- src/views/sections/upgrade.php
+++ src/views/sections/upgrade.php
@@ -2,7 +2,7 @@
 	<div class="section window">
 		<div class="item-container">
 			<div class="item">
-				<div class="search-configuration">
+				<div class="upgrade-configuration">
 					<div class="item-header">
 						<span class="button close icon icon-close"></span>
 						<h1>Request Upgrade</h1>
@@ -10,6 +10,7 @@
 					<div class="item-body">
 						<div class="message-container"></div>
 						<div class="upgrade-container"></div>
+						<div class="clear"></div>
 					</div>
 					<div class="item-footer">
 						<button class="button close alternate-button">Close</button>

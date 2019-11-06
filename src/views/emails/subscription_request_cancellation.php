faea67f35cffc4078a473c293aaf6c1ed58d4cb9
parsonsbots <parsonsbots@gmail.com>
2019-11-05 20:32:37 -0800

    subscription_request_cancellation email template

---
+++ src/views/emails/subscription_request_cancellation.php
@@ -0,0 +1,25 @@
+<?php
+	$message = 'Hello,' . "\n";
+	$message .= "\n";
+	$message .= 'This is a confirmation of your recent subscription cancellation request for your recurring subscription at ' . $this->settings['site_name'] . '.' . "\n";
+	$message .= "\n";
+	$message .= 'Subscription Details' . "\n";
+	$message .= '--' . "\n";
+	$message .= 'Subscription ID: #' . $templateParameters['subscription']['id'] . "\n";
+	$message .= 'Subscription Status: ' . ucwords($templateParameters['subscription']['status']) . "\n";
+	$message .= 'Recurring Payment Amount: ' . number_format($templateParameters['subscription']['price'], 2, '.', ',') . ' ' . $this->settings['billing']['currency'] . "\n";
+	$message .= 'Recurring Payment Interval: ' . $templateParameters['subscription']['interval_value'] . ' ' . $templateParameters['subscription']['interval_type'] . ($templateParameters['subscription']['interval_value'] !== 1 ? 's' : '') . "\n";
+	$message .= "\n";
+	$message .= 'User Details' . "\n";
+	$message .= '--' . "\n";
+	$message .= 'User Email: ' . $templateParameters['user']['email'] . "\n";
+	$message .= "\n";
+	$message .= 'If you\'d like to create a new subscription or reverse this cancellation request, please reply to this email.' . "\n";
+	$message .= "\n";
+	$message .= '--' . "\n";
+	$message .= $this->settings['site_name'] . "\n";
+	$message .= $domain . "\n";
+	$message .= $this->settings['from_email'] . "\n";
+	$message .= "\n";
+	$message .= date('M d, Y g:ia', time()) . ' ' . $this->settings['timezone']['display'];
+?>

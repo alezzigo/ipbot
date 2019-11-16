8cac3f38ad130229467fe16f1ba33f4161e75b38
parsonsbots <parsonsbots@gmail.com>
2019-11-16 14:45:23 -0800

    Add view for proxy API configuration

---
+++ src/views/sections/api.php
@@ -0,0 +1,23 @@
+<div class="hidden window-container" window="api">
+	<div class="section window">
+		<div class="item-container">
+			<div class="item">
+				<div class="api-configuration">
+					<div class="item-header">
+						<span class="button close icon icon-close"></span>
+						<h1>Proxy API Configuration</h1>
+					</div>
+					<div class="item-body">
+						<!-- .. -->
+						<div class="clear"></div>
+					</div>
+					<div class="item-footer">
+						<button class="button close alternate-button">Close</button>
+						<button class="button close main-button submit" process="proxies" window="api">Apply Configuration</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="window-overlay"></div>
+</div>

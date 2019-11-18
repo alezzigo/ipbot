dcd345a23b9d32859aabb8a062acce44b614d55e
parsonsbots <parsonsbots@gmail.com>
2019-11-17 19:14:44 -0800

    Change order of processEndpoint function

--- src/resources/js/proxies.js
+++ src/resources/js/proxies.js
@@ -6,45 +6,6 @@ var itemGrid = [];
 var itemGridCount = 0;
 var messageContainer = document.querySelector('main .message-container');
 var previousAction = 'fetch';
-var processEndpoint = function(frameName, frameSelector) {
-	requestParameters.action = 'endpoint';
-	requestParameters.data.order_id = document.querySelector('input[name="order_id"]').value;
-	requestParameters.table = 'orders';
-	requestParameters.url = '/api/orders';
-	sendRequest(function(response) {
-		if (messageContainer) {
-			messageContainer.innerHTML = (typeof response.message !== 'undefined' && response.message.text ? '<p class="message' + (response.message.status ? ' ' + response.message.status : '') + '">' + response.message.text + '</p>' : '');
-			processWindowEvents('resize');
-		}
-
-		if (response.data) {
-			var endpointEnableCheckboxInput = document.querySelector('.endpoint-enable');
-			var endpointEnableCheckboxLabel = document.querySelector('label[for="endpoint-enable"]');
-			endpointEnableCheckboxInput.removeEventListener('click', endpointEnableCheckboxInput.clickListener);
-			endpointEnableCheckboxLabel.removeEventListener('click', endpointEnableCheckboxLabel.clickListener);
-			endpointEnableCheckboxInput.clickListener = endpointEnableCheckboxLabel.clickListener = function() {
-				if (+endpointEnableCheckboxInput.getAttribute('checked')) {
-					elements.removeClass('.endpoint-enabled-container', 'hidden');
-				} else {
-					elements.addClass('.endpoint-enabled-container', 'hidden');
-				}
-			};
-			endpointEnableCheckboxInput.addEventListener('click', endpointEnableCheckboxInput.clickListener);
-			endpointEnableCheckboxLabel.addEventListener('click', endpointEnableCheckboxLabel.clickListener);
-
-			if (response.data.api_enable) {
-				elements.removeClass('.endpoint-enabled-container', 'hidden');
-				elements.setAttribute('.endpoint-enable', 'checked', +response.data.api_enable);
-				elements.setAttribute('.endpoint-username', 'value', response.data.api_username);
-				elements.setAttribute('.endpoint-password', 'value', response.data.api_password);
-				elements.html('.endpoint-whitelisted-ips', response.data.api_whitelisted_ips);
-			}
-		}
-
-		requestParameters.table = defaultTable;
-		requestParameters.url = defaultUrl;
-	});
-};
 var processCopy = function(frameName, frameSelector) {
 	previousAction = requestParameters.action;
 	var processCopyFormat = function() {
@@ -133,6 +94,45 @@ var processDowngrade = function() {
 		}
 
 		downgradeContainer.innerHTML = downgradeData;
+	});
+};
+var processEndpoint = function(frameName, frameSelector) {
+	requestParameters.action = 'endpoint';
+	requestParameters.data.order_id = document.querySelector('input[name="order_id"]').value;
+	requestParameters.table = 'orders';
+	requestParameters.url = '/api/orders';
+	sendRequest(function(response) {
+		if (messageContainer) {
+			messageContainer.innerHTML = (typeof response.message !== 'undefined' && response.message.text ? '<p class="message' + (response.message.status ? ' ' + response.message.status : '') + '">' + response.message.text + '</p>' : '');
+			processWindowEvents('resize');
+		}
+
+		if (response.data) {
+			var endpointEnableCheckboxInput = document.querySelector('.endpoint-enable');
+			var endpointEnableCheckboxLabel = document.querySelector('label[for="endpoint-enable"]');
+			endpointEnableCheckboxInput.removeEventListener('click', endpointEnableCheckboxInput.clickListener);
+			endpointEnableCheckboxLabel.removeEventListener('click', endpointEnableCheckboxLabel.clickListener);
+			endpointEnableCheckboxInput.clickListener = endpointEnableCheckboxLabel.clickListener = function() {
+				if (+endpointEnableCheckboxInput.getAttribute('checked')) {
+					elements.removeClass('.endpoint-enabled-container', 'hidden');
+				} else {
+					elements.addClass('.endpoint-enabled-container', 'hidden');
+				}
+			};
+			endpointEnableCheckboxInput.addEventListener('click', endpointEnableCheckboxInput.clickListener);
+			endpointEnableCheckboxLabel.addEventListener('click', endpointEnableCheckboxLabel.clickListener);
+
+			if (response.data.endpoint_enable) {
+				elements.removeClass('.endpoint-enabled-container', 'hidden');
+				elements.setAttribute('.endpoint-enable', 'checked', +response.data.endpoint_enable);
+				elements.setAttribute('.endpoint-username', 'value', response.data.endpoint_username);
+				elements.setAttribute('.endpoint-password', 'value', response.data.endpoint_password);
+				elements.html('.endpoint-whitelisted-ips', response.data.endpoint_whitelisted_ips);
+			}
+		}
+
+		requestParameters.table = defaultTable;
+		requestParameters.url = defaultUrl;
 	});
 };
 var processGroup = function(frameName, frameSelector) {

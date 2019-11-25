20c66e234c430fdce2bb584c9a2df94887eade83
parsonsbots <parsonsbots@gmail.com>
2019-11-24 20:59:08 -0800

    Enable user registration

--- src/views/sections/register.php
+++ src/views/sections/register.php
@@ -19,7 +19,7 @@
 					</div>
 					<div class="item-footer">
 						<button class="button close alternate-button">Close</button>
-						<button class="button main-button submit" disabled process="users" frame="register">Registration Disabled</button>
+						<button class="button main-button submit" process="users" frame="register">Register</button>
 					</div>
 				</div>
 			</div>

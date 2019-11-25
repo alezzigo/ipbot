7a9d75acdce7293aea379daf1c442adee38ade99
parsonsbots <parsonsbots@gmail.com>
2019-11-25 15:26:56 -0800

    About page revisions

--- src/views/pages/about.php
+++ src/views/pages/about.php
@@ -9,7 +9,7 @@
 		<div class="container small">
 			<h1>About Eightomic Proxies</h1>
 			<div class="section">
-				<h2>What is a Proxy Server?</h2>
+				<h2>What's a Proxy Server?</h2>
 				<p>A proxy server is an intermediary connection with its own public IP address between your device and its connection to the internet.</p>
 				<h2>Cloud Infrastructure With Static Dedicated IPs</h2>
 				<p>Scale any number of your application's dynamic cloud server IPs through the Eightomic proxy network using <a href="<?php echo $config->settings['base_url']; ?>static-proxies">static HTTP proxy IPs</a>.</p>

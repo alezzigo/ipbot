88e7567c38e65e254bca0bb8ef4495a4db850973
parsonsbots <parsonsbots@gmail.com>
2019-11-24 16:39:13 -0800

    Add more FAQs

--- src/views/pages/faq.php
+++ src/views/pages/faq.php
@@ -9,10 +9,22 @@
 		<div class="container small">
 			<h1>FAQs</h1>
 			<div class="section">
-				<h2>Do your proxies support SOCKS?</h2>
-				<p>Only HTTP / HTTPS is supported.</p>
+				<h2>Do you provide datacenter or residential proxies?</h2>
+				<p>You can start with a small quantity of proxies to determine the proxy type for your use case.</p>
 				<h2>Do you have a free trial?</h2>
 				<p>No, but there is an 8-day <a href="<?php echo $config->settings['base_url']; ?>refunds">refund policy</a>.</p>
+				<h2>Do your proxies block any specific websites?</h2>
+				<p>No.</p>
+				<h2>Do you provide backconnect proxies?</h2>
+				<p>No, the proxies are provided with direct IP:port connections.</p>
+				<h2>How often do you allow proxy IP replacements?</h2>
+				<p>Once every 2 weeks.</p>
+				<h2>Do you allow proxy IP location targeting?</h2>
+				<p>Due to varying IPv4 geolocation results, the proxies currently aren't guaranteed to be specific to any location.</p>
+				<h2>Do your proxies support SOCKS?</h2>
+				<p>Only HTTP / HTTPS is supported.</p>
+				<h2>Can you provide a discount code?</h2>
+				<p>No, quality is expensive.</p>
 			</div>
 		</div>
 	</div>

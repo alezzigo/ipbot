90c34212e8b17bb240105e244a21a7c911b2d9e7
parsonsbots <parsonsbots@gmail.com>
2019-11-25 15:19:07 -0800

    About page revisions

--- src/views/pages/about.php
+++ src/views/pages/about.php
@@ -12,9 +12,9 @@
 				<h2>What is a Proxy Server?</h2>
 				<p>A proxy server is an intermediary connection with its own public IP address between your device and its connection to the internet.</p>
 				<h2>Cloud Infrastructure With Static Dedicated IPs</h2>
-				<p>Scale any number of your application's cloud server IPs through a static HTTP proxy with programmatic API authentication and IP whitelisting.</p>
+				<p>Scale any number of your application's dynamic cloud server IPs through the Eightomic proxy network using <a href="<?php echo $config->settings['base_url']; ?>static-proxies">static HTTP proxy IPs</a>.</p>
 				<h2>Network Infrastructure With Granular Control</h2>
-				<p>Provide and manage private access to multiple users on your network using convenient proxy authentication features.</p>
+				<p>Provide and manage convenient access for multiple private users on your network using programmatic API authentication and IP whitelisting.</p>
 				<h2>Internet Privacy Through Obscurity</h2>
 				<p>Browse with multiple static dedicated HTTP proxies to change your IP address as often as you'd like. Decrease your IP-based footprint from websites and cookie tracking.</p>
 			</div>

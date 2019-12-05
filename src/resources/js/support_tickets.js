e33e66176ed1fe19e96dfc1ad4782f5e25fb4a4a
parsonsbots <parsonsbots@gmail.com>
2019-12-05 14:13:52 -0800

    Add JS file for tickets setup

---
+++ src/resources/js/support_tickets.js
@@ -0,0 +1,47 @@
+'use_strict';
+
+var defaultTable = 'support_tickets';
+var previousAction = 'fetch';
+var processSupportTicket = function() {
+	requestParameters.action = 'view';
+	requestParameters.table = 'support_tickets';
+	requestParameters.url = '/api/support_tickets';
+	var supportTicketContainer = document.querySelector('.support-ticket-container');
+	var supportTicketData = '';
+	var supportTicketId = document.querySelector('input[name="support_ticket_id"]').value;
+	requestParameters.conditions = {
+		id: supportTicketId
+	};
+	sendRequest(function(response) {
+		var messageContainer = document.querySelector('main .message-container');
+
+		if (messageContainer) {
+			messageContainer.innerHTML = (typeof response.message !== 'undefined' && response.message.text ? '<p class="message' + (response.message.status ? ' ' + response.message.status : '') + '">' + response.message.text + '</p>' : '');
+		}
+
+		if (
+			typeof response.redirect === 'string' &&
+			response.redirect
+		) {
+			window.location.href = response.redirect;
+			return false;
+		}
+
+		// ..
+	});
+};
+var processSupportTickets = function() {
+	requestParameters.action = 'list';
+	requestParameters.table = 'support_tickets';
+	requestParameters.url = '/api/support_tickets';
+	var supportTicketData = '';
+	sendRequest(function(response) {
+		var messageContainer = document.querySelector('main .message-container');
+
+		if (messageContainer) {
+			messageContainer.innerHTML = (typeof response.message !== 'undefined' && response.message.text ? '<p class="message' + (response.message.status ? ' ' + response.message.status : '') + '">' + response.message.text + '</p>' : '');
+		}
+
+		// ..
+	});
+};
